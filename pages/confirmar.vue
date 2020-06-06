f023763d7987e1dac9ffd5f4f9b07452a291dc4c
jose angel gamez <spartan102gamez@gmail.com>
2020-06-06 00:01:50 -0500

    Añadido boton de reenvio

--- pages/confirmar.vue
+++ pages/confirmar.vue
@@ -5,7 +5,16 @@
                 Bienvenido  {{nombre}}<br> solo un paso más
             </div>
             <div class="tarjeta-texto">
+                Gracias por registrarse en human+: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, velit?
+            </div>
+            <div class="tarjeta-texto">
                 Confirme su correo electrónico: Revise la bandeja de entrada y acceda al enlace que aparece en su interior.
+            </div>
+            <span class="texto-rojo letra-chica">
+                *si no ha recibido ningun correo de click en el siguiente boton
+            </span>
+            <div  class="tarjeta-boton">
+                Reenviar correo electrónico
             </div>
         </div>
     </div>
@@ -51,10 +60,31 @@ export default {
 }
 .tarjeta-titulo{
     font-size: 25px;
-    border-bottom: 2px solid black;
     padding-bottom: 40px;
+    border-bottom: 2px solid black;
 }
 .tarjeta-texto{
     padding: 20px 20px;
+    margin-bottom: 20px;
 }
+.tarjeta-boton{
+    padding: 5px 10px ;
+    margin: 20px;
+    background: skyblue;   
+    box-shadow:1px 1px 5px 1px black ;
+}
+.letra-chica{
+    font-size: 10px;
+}
+@media (min-width: 600px) {
+    .tarjeta{
+        width: 80%;
+    }
+}
+@media (min-width: 900px) {
+    .tarjeta{
+        width: 50%;
+    }
+    
+}      
 </style>
